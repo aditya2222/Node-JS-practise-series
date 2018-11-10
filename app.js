e6ba965fccab8a5caab406e4b251cd25d050fc28
Aditya <adityasingh222247@gmail.com>
2018-11-10 21:53:23 +0530

    Sending Responses

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const http = require('http')
 
 
 const server = http.createServer((req, res) => {
-    console.log(req, res)
+    console.log(req.url, req.method, req.headers)
     // Process.exit() is used to terminate the process
-    process.exit()
+    res.setHeader('CONTENT-TYPE', 'text/html')
+    res.write('<html>');
+    res.write('<head><title>My FIrst Page</title></head>');
+    res.write('<body><h1>Hello from my Node JS</h1></body>');
+    res.write('</html>');
+    res.end()
+
 })
 
 
