090f4e113e8fe33063d3db1a6eff5f056466ce00
Aditya <adityasingh222247@gmail.com>
2018-11-10 21:36:50 +0530

    The node lifecycle and event loop

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const http = require('http')
 
 const server = http.createServer((req, res) => {
     console.log(req, res)
+    // Process.exit() is used to terminate the process
+    process.exit()
 })
 
 
