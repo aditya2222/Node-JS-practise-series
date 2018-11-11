0520b7716134df0190e22499917dce3e1b0db85f
Aditya <adityasingh222247@gmail.com>
2018-11-11 13:41:50 +0530

    Using the node modules system

--- app.js
+++ app.js
@@ -1,42 +1,7 @@
 const http = require('http')
-const fs = require('fs')
+const routes = require('./routes')
 
-const server = http.createServer((req, res) => {
-    const url = req.url
-    const method = req.method
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title>Enter a Message</title></head>');
-        res.write('<body><form action="/message" method="post"><input name="message" type="text"/><button type="submit">Send</button></form></body>')
-        res.write('</html>');
-        return res.end()
-    }
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk)
-            body.push(chunk)
-        })
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString()
-            const message = parsedBody.split('=')[1]
-            console.log(message)
-            fs.writeFileSync('message.txt', message)
-        })
-        fs.writeFileSync('message.txt', 'DUMMY')
-        res.statusCode = 302;
-        res.setHeader('Location', '/')
-        return res.end()
-    }
-    // Process.exit() is used to terminate the process
-    res.setHeader('CONTENT-TYPE', 'text/html')
-    res.write('<html>');
-    res.write('<head><title>First Page</title></head>');
-    res.write('<body><h1>Hello World from Node JS</h1></body>')
-    res.write('</html>');
-    res.end()
-
-})
+const server = http.createServer(routes)
 
 
 server.listen(3000)
---
+++ routes.js
@@ -0,0 +1,53 @@
+const fs = require('fs')
+
+const requestHandler = (req, res) => {
+    const url = req.url
+    const method = req.method
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter a Message</title></head>');
+        res.write('<body><form action="/message" method="post"><input name="message" type="text"/><button type="submit">Send</button></form></body>')
+        res.write('</html>');
+        return res.end()
+    }
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk)
+            body.push(chunk)
+        })
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString()
+            const message = parsedBody.split('=')[1]
+            console.log(message)
+            fs.writeFile('message.txt', message, (error) => {
+                if (error) {
+                    console.log(error)
+                }
+                else {
+                    res.statusCode = 302;
+                    res.setHeader('Location', '/')
+                    return res.end()
+                }
+            })
+
+        })
+
+    }
+    // Process.exit() is used to terminate the process
+    res.setHeader('CONTENT-TYPE', 'text/html')
+    res.write('<html>');
+    res.write('<head><title>First Page</title></head>');
+    res.write('<body><h1>Hello World from Node JS</h1></body>')
+    res.write('</html>');
+    res.end()
+}
+
+
+module.exports = requestHandler
+
+// module.exports = {
+//     handler: requestHandler
+// }
+
+// module.exports.handler = requestHandler
