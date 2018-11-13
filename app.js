388cc3a2d7633e8dadb539ea2946aec493adb184
Aditya <adityasingh222247@gmail.com>
2018-11-13 22:22:24 +0530

    Using Express middlewares

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const express = require('express')
 
 const app = express()
 
+app.use((req, res, next) => {
+    // next() allows the request to continue to the next middleware in line
+    next()
+})
+
+
+app.use((req, res, next) => {
+    res.send('<h1>Hello from express </h1>')
+})
+
 const server = http.createServer(app)
 
 server.listen(3000)
