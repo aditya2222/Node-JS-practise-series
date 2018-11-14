bcf5ff0b725024cf1e8bb1dd13b6418910e55767
Aditya <adityasingh222247@gmail.com>
2018-11-14 18:37:25 +0530

    Serving HTML pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,3 +1,6 @@
+
+const path = require('path')
+
 const express = require("express")
 
 const router = express.Router()
@@ -5,7 +8,7 @@ const router = express.Router()
 // /admin/add-product
 
 router.get('/add-product', (req, res, next) => {
-    res.send("<form action='/admin/add-product' method='post'><input type='text' name='title' /><button type='submit'>Add Product</button></form>")
+    res.sendfile(path.join(__dirname, '../', 'views', 'add-product.html'))
 })
 
 // /admin/add-product
