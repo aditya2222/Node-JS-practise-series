1983f4a03b7ec9cae2f4db660a856fc9dabed0a6
Aditya <adityasingh222247@gmail.com>
2018-11-15 18:19:16 +0530

    Outputting Dynamic Content

--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,7 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
-const products = [];
+const products = []
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
@@ -15,7 +15,7 @@ router.get('/add-product', (req, res, next) => {
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
+  products.push({ title: req.body.title })
   res.redirect('/');
 });
 
