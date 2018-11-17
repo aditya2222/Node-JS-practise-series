675e11fe408edf0d3cae742426c083d8f9e2c0be
Aditya <adityasingh222247@gmail.com>
2018-11-17 13:04:46 +0530

    Working with EJS

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ const app = express();
 // Specifying pug as our templating engine
 // app.set('view engine', 'pug')
 // Specifying handlebars as our templating engine
-app.engine('handlebars', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout'}));
-app.set('view engine', 'handlebars');
+// app.engine('handlebars', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout'}));
+// app.set('view engine', 'handlebars');
+// Setting up ejs as the view engine
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
