4fd988ca4943e486ea9e535cc7ea7b681a37f75d
Aditya <adityasingh222247@gmail.com>
2018-11-17 22:16:10 +0530

    Storing data into file via models

--- models/product.js
+++ models/product.js
@@ -1,16 +1,32 @@
-const products = []
-
+const fs = require('fs')
+const path = require('path')
 module.exports = class Product {
     constructor(t) {
         this.title = t
     }
 
     save() {
-        products.push(this)
+        const myPath = path.join(__dirname, '../', 'data', 'products.json')
+        fs.readFile(myPath, (err, data) => {
+            let products = []
+            if (!err) {
+                products = JSON.parse(data)
+            }
+            products.push(this)
+            fs.writeFile(myPath, JSON.stringify(products), (error) => {
+                console.log(error)
+            })
+        })
     }
 
     static fetchAll() {
-        return products
+        const myPath = path.join(__dirname, '../', 'data', 'products.json')
+        fs.readFile(myPath, (err, data) => {
+            if (err) {
+                return [];
+            }
+            return JSON.parse(data)
+        })
     }
 
 
