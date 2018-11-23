499f4368e4d2b6525eade4d762385b72b2daa1d8
Aditya <adityasingh222247@gmail.com>
2018-11-23 11:34:34 +0530

    Adjustments for the new module

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const errorHandler = require('./controllers/error')
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
-// Specifying pug as our templating engine
-// app.set('view engine', 'pug')
-// Specifying handlebars as our templating engine
-// app.engine('handlebars', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout'}));
-// app.set('view engine', 'handlebars');
-// Setting up ejs as the view engine
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -23,6 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use(errorHandler.get404);
+app.use(errorController.get404);
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -4,10 +4,6 @@ const express = require('express');
 
 const shopController = require('../controllers/shop');
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
-
-
 const router = express.Router();
 
 router.get('/', shopController.getIndex);
@@ -16,9 +12,8 @@ router.get('/products', shopController.getProducts);
 
 router.get('/cart', shopController.getCart);
 
-router.get('/checkout', shopController.getCheckout);
-
 router.get('/orders', shopController.getOrders);
 
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
