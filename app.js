c032286cb7870492f9c1d0f4fc8c621a4bd47b7a
Aditya <adityasingh222247@gmail.com>
2018-12-03 11:09:22 +0530

    Syncing JS Definitions to the database

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const db = require('./util/database')
+const sequelize = require('./util/database')
 
 const errorController = require('./controllers/error');
 
@@ -14,15 +14,6 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-db.execute('SELECT * FROM products')
-.then((result) => {
-	
-})
-.catch((error) => {
-	console.log(error)
-})
-
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -31,5 +22,13 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+sequelize.sync()
+.then((result) => {
+	app.listen(3000);
+})
+.catch((error) => {
+	console.log(error)
+})
 
-app.listen(3000);
+
+
