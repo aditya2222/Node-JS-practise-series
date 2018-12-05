28c10a0346c3a0c358751b60010c7e42071d6fa1
Aditya <adityasingh222247@gmail.com>
2018-12-05 12:14:47 +0530

    Creating and managing a dummy user

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
 	User.findById(1)
 		.then((user) => {
-			req.user = user
+			req.user = user;
+			next()
 		})
 		.catch((error) => {
 			console.log(error)
