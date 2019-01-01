99dd1d01cd5bc120f7bb58ab4d922fa24a4903cc
Aditya <adityasingh222247@gmail.com>
2019-01-01 12:35:08 +0530

    Getting and Displaying the orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -81,17 +81,17 @@ exports.postCartDelete = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-    req.user.getOrders()
-	.then((orders)=>{	
-	    res.render('shop/orders', {
-		path: '/orders',
-		pageTitle: 'Your Orders',
-		orders: orders
-	    });
-	})
-	.catch((error) => {
-		console.log(error)	
-	})
+	Order.find({'user.userId': req.user._id}) 
+		.then((orders)=>{	
+		    res.render('shop/orders', {
+			path: '/orders',
+			pageTitle: 'Your Orders',
+			orders: orders
+		    });
+		})
+		.catch((error) => {
+			console.log(error)	
+		})
 };
 
 exports.getCheckout = (req, res, next) => {
