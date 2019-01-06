359553012b1254fbc1a2273256d41cdf11e97059
Aditya <adityasingh222247@gmail.com>
2019-01-06 15:51:46 +0530

    Making add to cart work again

--- app.js
+++ app.js
@@ -38,20 +38,28 @@ app.use(session({
 
 }))
  
-//app.use((req, res, next) => {
-//  User.findById('5c28d7d6d1236114e2980b45')
-//    .then(user => {
-//      req.user = user;
-//      next();
-//    })
-//    .catch(err => console.log(err));
-//});
+
+
+app.use((req, res, next) => {
+	if(!req.session.user){
+		
+		return next()
+	
+	}
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes)
 
 app.use(errorController.get404);
+
 
 mongoose
   .connect(
