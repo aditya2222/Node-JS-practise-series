d6b89b1214cd8f78be8255e0fb31110fb3929027
Aditya <adityasingh222247@gmail.com>
2019-02-07 21:41:35 +0530

    Throwing Errors in Code

--- app.js
+++ app.js
@@ -49,10 +49,16 @@ app.use((req, res, next) => {
     }
     User.findById(req.session.user._id)
         .then(user => {
+		if(!user){	
+			return next();
+		}
             req.user = user;
             next();
         })
-        .catch(err => console.log(err));
+        .catch(err=>{	
+		throw new Error(err)
+		//next();
+	});
 });
 
 app.use((req, res, next) => {
