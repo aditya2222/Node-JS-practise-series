cc71886e9b2e56ddde00070214103b6d88d37a97
Aditya <adityasingh222247@gmail.com>
2019-02-17 15:34:05 +0530

    filtering files based on mime type

--- app.js
+++ app.js
@@ -11,23 +11,34 @@ const User = require('./models/user');
 const multer = require('multer')
 
 const MONGODB_URI =
-    'mongodb+srv://admin:tiktik123@cluster0-5t9yf.mongodb.net/shop';
+  'mongodb+srv://admin:tiktik123@cluster0-5t9yf.mongodb.net/shop';
 
 const app = express();
 const store = new MongoDBStore({
-    uri: MONGODB_URI,
-    collection: 'sessions'
+  uri: MONGODB_URI,
+  collection: 'sessions'
 });
 
 
 const fileStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'images')
-    },
-    filename: (req, file, cb) => {
-        cb(null, new Date().toISOString() + '-' + file.originalname)
-    },
+  destination: (req, file, cb) => {
+    cb(null, 'images')
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + '-' + file.originalname)
+  },
 })
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+    cb(null, true)
+  }
+  else {
+    cb(null, false)
+  }
+
+}
+
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -38,17 +49,17 @@ const authRoutes = require('./routes/auth');
 const csrfProtection = csrf();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ storage: fileStorage }).single('image'))
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'))
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use(
-    session({
-        secret: 'my secret',
-        resave: false,
-        saveUninitialized: false,
-        store: store
-    })
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
 );
 
 
@@ -56,31 +67,31 @@ app.use(csrfProtection)
 app.use(flash())
 app.use((req, res, next) => {
 
-    res.locals.isAuthenticated = req.session.isLoggedIn;
-    res.locals.csrfToken = req.csrfToken()
-    next()
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken()
+  next()
 })
 
 app.use((req, res, next) => {
-    // throw new Error('Sync Dummy')
-    if (!req.session.user) {
+  // throw new Error('Sync Dummy')
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      // throw new Error('Dummy')
+      if (!user) {
         return next();
-    }
-    User.findById(req.session.user._id)
-        .then(user => {
-            // throw new Error('Dummy')
-            if (!user) {
-                return next();
-            }
-            req.user = user;
-            next();
-        })
-        .catch(err => {
-            next(new Error(err))
-            // Does not work for errors inside async code such as inside then block
-            // throw new Error(err)
-            //next();
-        });
+      }
+      req.user = user;
+      next();
+    })
+    .catch(err => {
+      next(new Error(err))
+      // Does not work for errors inside async code such as inside then block
+      // throw new Error(err)
+      //next();
+    });
 });
 
 
@@ -93,20 +104,20 @@ app.get('/500', errorController.get505);
 // express automatically detects that below is a error handling middleware
 app.use((error, req, res, next) => {
 
-    res.status(404).render('500', {
-        pageTitle: 'Page Not Found',
-        path: '/505',
-        isAuthenticated: req.session.isLoggedIn
-    });
+  res.status(404).render('500', {
+    pageTitle: 'Page Not Found',
+    path: '/505',
+    isAuthenticated: req.session.isLoggedIn
+  });
 
 });
 
 mongoose
-    .connect(MONGODB_URI, { useNewUrlParser: true })
-    .then(result => {
-        console.log('Connected');
-        app.listen(3000);
-    })
-    .catch(err => {
-        console.log(err);
-    });
+  .connect(MONGODB_URI, { useNewUrlParser: true })
+  .then(result => {
+    console.log('Connected');
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
