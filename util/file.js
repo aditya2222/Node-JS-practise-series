c2a23790cb46ef326d50025c6dac072a646b0590
Aditya <adityasingh222247@gmail.com>
2019-02-23 11:24:56 +0530

    Deleting files

---
+++ util/file.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+
+const deleteFile = (filePath) => {
+
+    fs.unlink(filePath, (error) => {
+
+        if (error) {
+            throw (error)
+        }
+
+    })
+
+}
+
+exports.deleteFile = deleteFile;
