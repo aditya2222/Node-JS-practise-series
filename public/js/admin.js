803031c68a0038c5446c177c8b163494e114d1f5
aditya2222 <adityasingh222247@gmail.com>
2019-03-16 19:33:16 +0530

    manipulating the dom

--- public/js/admin.js
+++ public/js/admin.js
@@ -2,6 +2,8 @@ const deleteProduct = (btn) => {
 
     const prodId = btn.parentNode.querySelector('[name=productId]').value;
     const csrfToken = btn.parentNode.querySelector('[name=_csrf]').value
+    // clisest element
+    const productElement = btn.closest('article')
 
     fetch('/admin/product/' + prodId, {
         method: 'DELETE',
@@ -11,6 +13,11 @@ const deleteProduct = (btn) => {
     })
         .then((response) => {
             console.log(response);
+
+        })
+        .then((data) => {
+            console.log(data);
+            productElement.parentNode.removeChild(productElement)
 
         })
         .catch((error) => {
